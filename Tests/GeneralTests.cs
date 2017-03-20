2ab7c66fdab4e6c8800978e5364602c7f72f5023
ingen084 <ingen188@gmail.com>
2017-03-20 21:05:19 +0900

    スペース追加

--- Tests/GeneralTests.cs
+++ Tests/GeneralTests.cs
@@ -9,7 +9,7 @@ namespace Tests
 		[Fact(DisplayName = "UrlGenerator Test")]
 		public static void Urlgenerator()
 		{
-			Assert.Equal(UrlGenerator.Generate(UrlType.EewJson,DateTime.Parse("2017/03/19 22:13:47")), "http://www.kmoni.bosai.go.jp/new/webservice/hypo/eew/20170319221347.json");
+			Assert.Equal(UrlGenerator.Generate(UrlType.EewJson, DateTime.Parse("2017/03/19 22:13:47")), "http://www.kmoni.bosai.go.jp/new/webservice/hypo/eew/20170319221347.json");
 		}
 	}
 }
