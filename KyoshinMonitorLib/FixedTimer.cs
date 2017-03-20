83c669e7fdb84d35d139d1a612bc0935753b4fe4
ingen084 <ingen188@gmail.com>
2017-03-20 23:47:08 +0900

    FixedTimer追加

---
+++ KyoshinMonitorLib/FixedTimer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KyoshinMonitorLib
+{
+	/// <summary>
+	/// 誤差を蓄積させないようにするタイマー
+	/// </summary>
+	public class FixedTimer
+	{
+		private Timer _timer;
+		private DateTime _lastTime;
+
+		public TimeSpan _interval = TimeSpan.FromMilliseconds(1000);
+		/// <summary>
+		/// タイマーの間隔
+		/// </summary>
+		public TimeSpan Interval
+		{
+			get => _interval;
+			set
+			{
+				if (_accuracy.Ticks <= 0)
+					throw new ArgumentOutOfRangeException("Intervalには1Tick以上の時間を指定してください。");
+				_interval = value;
+			}
+		}
+
+		private TimeSpan _accuracy = TimeSpan.FromMilliseconds(1);
+		/// <summary>
+		/// タイマーの精度 ただし精度を保証するものではありません。
+		/// <para>上限がある上、精度を高くするとその分重くなります。</para>
+		/// <para>この値より短い単位でイベントが発行されることはありません。</para>
+		/// <para>Intervalよりも大きな値を指定した際、誤差が蓄積されない特性が消滅することに気をつけてください。</para>
+		/// </summary>
+		public TimeSpan Accuracy
+		{
+			get => _accuracy;
+			set
+			{
+				if (_accuracy.Ticks <= 0)
+					throw new ArgumentOutOfRangeException("Accuracyには1Tick以上の時間を指定してください。");
+				_accuracy = value;
+			}
+		}
+
+		/// <summary>
+		/// タイマーが1度実行された後
+		/// </summary>
+		public bool AutoReset { get; set; } = true;
+
+		public event Action Elapsed;
+
+
+		public FixedTimer(TimeSpan interval) : this()
+		{
+			Interval = interval;
+		}
+		public FixedTimer()
+		{
+			_timer = new Timer(s =>
+			{
+				if (DateTime.Now - _lastTime >= Interval)
+				{
+					ThreadPool.QueueUserWorkItem(s2 => Elapsed?.Invoke());
+					_lastTime += Interval;
+					if (!AutoReset)
+						_timer.Change(Timeout.Infinite, Timeout.Infinite);
+				}
+			}, null, Timeout.Infinite, Timeout.Infinite);
+		}
+
+		public void Start()
+		{
+			_lastTime = DateTime.Now;
+			_timer.Change(Accuracy, Accuracy);
+		}
+
+		public void Stop()
+			=> _timer.Change(Timeout.Infinite, Timeout.Infinite);
+	}
+}
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -12,25 +12,44 @@ namespace Tests
 	{
 		public static void Main(string[] args)
 		{
+			//ファイルがなかったら帰る
 			if (!File.Exists("ShindoObsPoints.pbf"))
 				return;
 
 			//観測点情報読み込み
 			var points = ObservationPoint.LoadFromPbf("ShindoObsPoints.pbf");
 
-			//時間計算(今回は適当にPC時間-5秒)
-			var time = DateTime.Now.AddSeconds(-5);
+			//誤差が蓄積しないタイマーのインスタンスを作成(デフォルトは間隔1000ms+精度1ms↓)
+			var timer = new FixedTimer();
 
-			//画像を取得して結果を計算
-			var result = points.CalculateIntensityFromParameterAsync(time, false).Result;
+			//適当にイベント設定
+			timer.Elapsed += () =>
+			{
+				//時間計算(今回は適当にPC時間-5秒)
+				var time = DateTime.Now.AddSeconds(-5);
 
-			//適当に一つ目の観測地点の震度
-			Console.WriteLine("FirstInt: " + result.First().AnalysisResult);
+				Console.WriteLine($"\n**{time.ToLongTimeString()}");
 
-			//現在の最大震度
-			Console.WriteLine("MaxInt: " + result.Max(r => r.AnalysisResult));
+				//画像を取得して結果を計算 (良い子のみんなはawaitを使おうね！)
+				var result = points.CalculateIntensityFromParameterAsync(time, false).Result;
 
+				//適当に一つ目の観測地点の震度
+				Console.WriteLine("FirstInt: " + result.First().AnalysisResult);
+
+				//現在の最大震度
+				Console.WriteLine("MaxInt: " + result.Max(r => r.AnalysisResult));
+			};
+
+			//タイマー開始
+			timer.Start();
+
+			Console.WriteLine("Enterキー入力で終了します。");
+
+			//改行入力待ち
 			Console.ReadLine();
+
+			//タイマー終了
+			timer.Stop();
 		}
 	}
 }
