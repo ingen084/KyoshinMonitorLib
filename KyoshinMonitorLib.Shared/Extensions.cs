4b9cbb12a026011d9790b6a10e7698504263de70
ingen084 <ingen188@gmail.com>
2017-04-18 19:40:20 +0900

    バグ修正

--- KyoshinMonitorLib.Shared/Extensions.cs
+++ KyoshinMonitorLib.Shared/Extensions.cs
@@ -21,8 +21,8 @@ namespace KyoshinMonitorLib
 		/// <param name="datetime">参照する日付</param>
 		/// <param name="isBehole">地中の情報を取得するかどうか</param>
 		/// <returns>震度情報が追加された観測点情報の配列</returns>
-		public static async Task<IEnumerable<ImageAnalysisResult>> ParseIntensityFromParameterAsync(this IEnumerable<ObservationPoint> points, DateTime datetime, bool isBehole = false)
-			=> await points.Select(p => new ImageAnalysisResult(p)).ParseIntensityFromParameterAsync(datetime, isBehole);
+		public static async Task<ImageAnalysisResult[]> ParseIntensityFromParameterAsync(this ObservationPoint[] points, DateTime datetime, bool isBehole = false)
+			=> await points.Select(p => new ImageAnalysisResult(p)).ToArray().ParseIntensityFromParameterAsync(datetime, isBehole);
 
 		/// <summary>
 		/// 与えられた情報から強震モニタの画像を取得し、そこから観測点情報を使用し震度を取得します。
@@ -32,7 +32,7 @@ namespace KyoshinMonitorLib
 		/// <param name="datetime">参照する日付</param>
 		/// <param name="isBehole">地中の情報を取得するかどうか</param>
 		/// <returns>震度情報が追加された観測点情報の配列</returns>
-		public static async Task<IEnumerable<ImageAnalysisResult>> ParseIntensityFromParameterAsync(this IEnumerable<ImageAnalysisResult> points, DateTime datetime, bool isBehole = false)
+		public static async Task<ImageAnalysisResult[]> ParseIntensityFromParameterAsync(this ImageAnalysisResult[] points, DateTime datetime, bool isBehole = false)
 		{
 			using (var client = new HttpClient())
 			using (var bitmap = new Bitmap(await client.GetStreamAsync(UrlGenerator.Generate(UrlType.RealTimeImg, datetime, RealTimeImgType.Shindo, isBehole))))
@@ -45,7 +45,7 @@ namespace KyoshinMonitorLib
 		/// <param name="points">使用する観測点情報の配列</param>
 		/// <param name="bitmap">参照する画像</param>
 		/// <returns>震度情報が追加された観測点情報の配列</returns>
-		public static IEnumerable<ImageAnalysisResult> ParseIntensityFromImage(this IEnumerable<ImageAnalysisResult> points, Bitmap bitmap)
+		public static ImageAnalysisResult[] ParseIntensityFromImage(this ImageAnalysisResult[] points, Bitmap bitmap)
 		{
 			foreach (var point in points)
 			{
