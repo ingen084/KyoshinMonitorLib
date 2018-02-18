66fd961e80ae126ccf670fb6a2dde463ffb0aa49
ingen084 <ingen188@gmail.com>
2018-02-19 04:13:52 +0900

    RealTimeData追加

---
+++ src/KyoshinMonitorLib/ApiResult/RealTimeData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace KyoshinMonitorLib.ApiResult
+{
+	public class RealTimeData
+	{
+		[DataMember(Name = "dataTime")]
+		public DateTime DateTime { get; set; }
+		[DataMember(Name = "packetType")]
+		public string PacketType { get; set; }
+		[DataMember(Name = "kyoshinType")]
+		public string KyoshinType { get; set; }
+		[DataMember(Name = "baseData")]
+		public string BaseData { get; set; }
+		[DataMember(Name = "baseSerialNo")]
+		public string BaseSerialNo { get; set; }
+		[DataMember(Name = "items")]
+		public float?[] Items { get; set; }
+		[DataMember(Name = "result")]
+		public Result Result { get; set; }
+		[DataMember(Name = "security")]
+		public Security Security { get; set; }
+	}
+
+	public class Result
+	{
+		[DataMember(Name = "status")]
+		public string Status { get; set; }
+		[DataMember(Name = "message")]
+		public string Message { get; set; }
+	}
+
+	public class Security
+	{
+		[DataMember(Name = "realm")]
+		public string Realm { get; set; }
+		[DataMember(Name = "hash")]
+		public string Hash { get; set; }
+	}
+
+}
